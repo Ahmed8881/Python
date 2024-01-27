d9a67a5d826565d4fc53978d1d8aff408f77b42d
M Ahmed Butt <144407474+Ahmed8881@users.noreply.github.com>
2024-01-27 09:22:05 +0500

    Update gcd.py

--- baasics/gcd.py
+++ baasics/gcd.py
@@ -2,7 +2,7 @@ def gcd(a, b):
     while b != 0:
         a, b = b, a % b
     return a
-
+,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,
 a=int(input("Enter first number: "))
 b=int(input("Enter second number: "))
 print(gcd(a,b))
